54663232977eb484aadc9836c282e166feb207c8
Mihwa_Kim <64059691+mihwa-kim@users.noreply.github.com>
2024-07-23 14:32:40 +1000

    automation projects added

---
+++ automation/backup.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = "/Users/gimmihwa/Desktop/LESSGO"
+destination_dir = "/Users/gimmihwa/Desktop/backup"
+
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+
+schedule.every().day.at("14:31").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
